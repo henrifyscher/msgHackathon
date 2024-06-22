f07e711bd9384e204fb5de25aea17759f846eb6d
Keck, Paul <paul.keck@stud.hs-coburg.de>
2024-06-22 09:14:51 +0200

    restruct

---
+++ backend/src/main/java/com/gruppe2/healthify/PingTest.java
@@ -0,0 +1,13 @@
+package com.gruppe2.healthify;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingTest {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
