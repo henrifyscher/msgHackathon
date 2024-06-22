58c523216930bbb78fddc495d1093c942f172459
Keck, Paul <paul.keck@stud.hs-coburg.de>
2024-06-22 09:37:24 +0200

    User added

---
+++ backend/src/main/java/com/gruppe2/healthify/entity/User.java
@@ -0,0 +1,23 @@
+package com.gruppe2.healthify.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
